3cbb74c8cba16360da104c0fa4a0997ab59ffc07
wangfucheng <wangfucheng@machloop.com>
2021-03-15 13:49:57 +0800

    feat: 按需加载 antd 的样式

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import visualizer from 'rollup-plugin-visualizer'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import visualizer from 'rollup-plugin-visualizer';
+import vitePluginImp from 'vite-plugin-imp';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), []],
+  plugins: [
+    reactRefresh(),
+    // 按需加载 antd 样式
+    // @see: https://github.com/vitejs/vite/issues/1389
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
+  ],
   css: {
     preprocessorOptions: {
       less: {
@@ -17,4 +30,4 @@ export default defineConfig({
       plugins: [visualizer()],
     },
   },
-})
+});
